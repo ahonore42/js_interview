a455c618b822dd0a096b69a5cba0088891aaff99
ahonore42 <a1honore80@gmail.com>
2021-01-11 10:21:04 -0600

    add clear final instruction

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // A function has been provided for you, it accept's two arguments, an input and a char,
 // the input being the users input and the char being the character to remove from the input. 
 // The function should also check if the input or char is valid, no empty values or numbers are allowed, 
-// should return an empty string if the input or char is invalid
+// should return an empty string if the input or char is invalid.
+// Otherwise, return the input with the char removed.
 
 const parseInput = (input,char) => {
   // Write your code here
